3a8015f48753bb196a428ac6c525d464c5ea142e
Leo_98 <82750536+leo-knight98@users.noreply.github.com>
2021-10-08 10:58:35 +0200

    Add files via upload

--- vistas/usuarios/login.php
+++ vistas/usuarios/login.php
@@ -1,1 +1,21 @@
-
+<div class="container">
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div class="col">
+            <div class="card h-100">
+                <div class="card-body">
+                    <form method="post">
+                        <div class="mb-3">
+                            <label for="nombre" class="form-label">Nombre de usuario:</label>
+                            <input type="text" name="nombre" id="nombre" class="form-control" placeholder="" aria-describedby="helpId">
+                        </div>
+                        <div class="mb-3">
+                            <label for="pass" class="form-label">Contraseña:</label>
+                            <input type="password" name="pass" id="pass" class="form-control" placeholder="" aria-describedby="helpId">
+                        </div>
+                        <button type="submit" class="btn btn-success">Entrar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
